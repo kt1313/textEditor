93d2e1875da1dc7e447a2aef6f2575e4b62b08b7
kt1313 <tomaklim@wp.pl>
2020-12-15 23:09:43 +0100

    WspolnyEdytor - gotowy, ze stromy:(

--- src/main/java/pl/javastart/textEditor/Editor.java
+++ src/main/java/pl/javastart/textEditor/Editor.java
@@ -1,15 +1,31 @@
 package pl.javastart.textEditor;
+
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
 import java.io.IOException;
-@WebServlet("")
 
-public class Editor  extends HttpServlet {
+@WebServlet("")
+public class Editor extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("text", "Text from servlet");
+        ServletContext context = getServletContext();
+        Object text = context.getAttribute("text");
+        forwardRequestWithTextAttribute(request, response, text);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String text = request.getParameter("text");
+        ServletContext context = getServletContext();
+        context.setAttribute("text", text);
+        forwardRequestWithTextAttribute(request, response, text);
+    }
+
+    private void forwardRequestWithTextAttribute(HttpServletRequest request, HttpServletResponse response, Object text) throws ServletException, IOException {
+        request.setAttribute("text", text != null ? text : "");
         request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 }
